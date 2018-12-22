38cabd055deee0e13fb03145b53d1205d3f67723
Bruno Leitão <bleitao@ons.org.br>
2018-12-22 21:26:35 -0200

    'form'

--- interface/src/forms/BrewSettingsForm.js
+++ interface/src/forms/BrewSettingsForm.js
@@ -3,6 +3,7 @@ import FormGroup from '@material-ui/core/FormGroup';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 
 class BrewSettingsForm extends Component {
   constructor() {
@@ -10,29 +11,90 @@ class BrewSettingsForm extends Component {
 
     this.state = {
       boilPercent: 90,
+      sampleTime: 1,
       kP: 100,
       kI: 100,
       kD: 100
     }
   }
+
+  handleBoilPercent = (e) => {
+    this.setState({ boilPercent: e.target.value })
+  }
+
+  handlesampleTime = (e) => {
+    this.setState({ sampleTime: e.target.value })
+  }
+
+  handleKp = (e) => {
+    this.setState({ kP: e.target.value })
+  }
+
+  handleKi = (e) => {
+    this.setState({ kI: e.target.value })
+  }
+
+  handleKd = (e) => {
+    this.setState({ kD: e.target.value })
+  }
+
   render() {
     return (
-      <FormGroup>
-        <TextField required label="Boil %" type="number" margin="normal" fullWidth
+      <ValidatorForm ref="form" onSubmit={this.saveSettings} onError={error => console.log(error)}>
+        <TextValidator 
+          name="boilPercent"
+          validators={['required']}  
+          label="Boil %" 
+          type="number" 
+          fullWidth
           InputProps={{ endAdornment: <InputAdornment position="start">%</InputAdornment> }}
-          value={this.state.boilPercent} onChange={this.handeTimeChange}
+          value={this.state.boilPercent} 
+          onChange={this.handleBoilPercent}
+          errorMessages={['this field is required']}
         />
-        <TextField required label="kP" type="number" margin="normal" fullWidth
-          value={this.state.kP} onChange={this.handeTimeChange}
+        <TextValidator 
+          name="sampleTime"
+          validators={['required']}  
+          label="Sample Time PID" 
+          type="number" 
+          fullWidth
+          InputProps={{ endAdornment: <InputAdornment position="start">sec</InputAdornment> }}
+          value={this.state.sampleTime} 
+          onChange={this.handleSampleTime}
+          errorMessages={['this field is required']}
         />
-        <TextField required label="kI" type="number" margin="normal" fullWidth
-          value={this.state.kI} onChange={this.handeTimeChange}
+        <TextValidator 
+          name="kp"
+          label="kP"
+          type="number"
+          validators={['required']}  
+          fullWidth
+          value={this.state.kP} 
+          onChange={this.handleKp}
+          errorMessages={['this field is required']}
         />
-        <TextField required label="kD" type="number" margin="normal" fullWidth
-          value={this.state.kD} onChange={this.handeTimeChange}
+        <TextValidator 
+          name="ki"
+          label="kI"
+          type="number"
+          validators={['required']}
+          fullWidth
+          value={this.state.kI}
+          onChange={this.handleKi}
+          errorMessages={['this field is required']}
         />
-        <Button variant="contained" color="primary" onClick={this.addItem}>Save</Button>
-      </FormGroup>
+        <TextValidator 
+          name="kd"
+          label="kD"
+          type="number"
+          validators={['required']}
+          fullWidth
+          value={this.state.kD}
+          onChange={this.handleKd}
+          errorMessages={['this field is required']}
+        />
+        <Button type="submit" variant="contained" fullWidth color="primary" onClick={this.addItem}>Save</Button>
+      </ValidatorForm>
     )
   }
 }
