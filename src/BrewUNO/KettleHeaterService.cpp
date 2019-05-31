38492d81947920b68fed8338b6909d90c9633de8
Bruno Leitão <bleitao@ons.org.br>
2019-05-31 18:18:37 -0300

    'pwm'

--- src/BrewUNO/KettleHeaterService.cpp
+++ src/BrewUNO/KettleHeaterService.cpp
@@ -50,7 +50,7 @@ void KettleHeaterService::Compute()
 
   if (_activeStatus->ActiveStep == boil)
   {
-    _activeStatus->PWM = ((1023 * _brewSettingsService->BoilPowerPercentage) / 100);
+    _activeStatus->PWM = ((1023 * _activeStatus->BoilPowerPercentage) / 100);
     analogWrite(HEATER_BUS, _activeStatus->PWM);
     return;
   }
