995da53121ce74cdd80349964ecedec679ace47f
Bruno Leitão <bleitao@ons.org.br>
2019-06-14 20:08:42 -0300

    ''

--- src/BrewUNO/KettleHeaterService.cpp
+++ src/BrewUNO/KettleHeaterService.cpp
@@ -93,9 +93,10 @@ void KettleHeaterService::Compute()
   else
     _kettlePID.Compute();
 
+  int MaxPWM = ((1023 * _brewSettingsService->MashHeaterPercentage) / 100);
+  _activeStatus->PWM = KettleOutput > MaxPWM ? MaxPWM : KettleOutput;
   Serial.print("OUTPUT: ");
-  Serial.println(KettleOutput);
+  Serial.println(_activeStatus->PWM);
 
-  _activeStatus->PWM = KettleOutput;
-  analogWrite(HEATER_BUS, KettleOutput);
+  analogWrite(HEATER_BUS, _activeStatus->PWM);
 }
