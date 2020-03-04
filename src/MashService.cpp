5e6b971bfe8a1c8a0f5797f7a9bc2fcb28e992c1
Eduardo 'dumpa' Sanches <34325697+dumpamob@users.noreply.github.com>
2020-03-04 14:30:53 -0300

    pump revision

--- src/MashService.cpp
+++ src/MashService.cpp
@@ -37,10 +37,11 @@ void MashService::loop(ActiveStatus *activeStatus)
         activeStatus->FullPower = ((int)steps[0]["fp"]) == 1;
         activeStatus->ActiveMashStepName = steps[0]["n"].as<String>();
         activeStatus->ActiveMashStepSufixName = getMashName(activeStatus, steps[0]);
-        activeStatus->Recirculation = ((int)steps[0]["r"]) == 1;
-        if (activeStatus->Recirculation) {
-        _pump->TurnPumpOn();
-        }
+        activeStatus-> Recirculation = ((int) steps [0] ["r"]) == 1;
+        if (activeStatus-> Recirculation)
+            _pump-> TurnPumpOn ();
+        else
+            _pump-> TurnPumpOff ();
 
         // brew was stopped during anti cavitatiton
         if (!activeStatus->BrewStarted || activeStatus->ActiveStep != mash)
