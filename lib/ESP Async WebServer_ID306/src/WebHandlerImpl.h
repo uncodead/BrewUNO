48bf19c1c78682b665e678cbba474a715bba76e3
Leitao <uncodead@gmail.com>
2020-06-25 23:56:08 -0300

    refac

--- lib/ESP Async WebServer_ID306/src/WebHandlerImpl.h
+++ lib/ESP Async WebServer_ID306/src/WebHandlerImpl.h
@@ -21,90 +21,108 @@
 #ifndef ASYNCWEBSERVERHANDLERIMPL_H_
 #define ASYNCWEBSERVERHANDLERIMPL_H_
 
-
 #include "stddef.h"
 #include <time.h>
 
-class AsyncStaticWebHandler: public AsyncWebHandler {
-   using File = fs::File;
-   using FS = fs::FS;
-  private:
-    bool _getFile(AsyncWebServerRequest *request);
-    bool _fileExists(AsyncWebServerRequest *request, const String& path);
-    uint8_t _countBits(const uint8_t value) const;
-  protected:
-    FS _fs;
-    String _uri;
-    String _path;
-    String _default_file;
-    String _cache_control;
-    String _last_modified;
-    AwsTemplateProcessor _callback;
-    bool _isDir;
-    bool _gzipFirst;
-    uint8_t _gzipStats;
-  public:
-    AsyncStaticWebHandler(const char* uri, FS& fs, const char* path, const char* cache_control);
-    virtual bool canHandle(AsyncWebServerRequest *request) override final;
-    virtual void handleRequest(AsyncWebServerRequest *request) override final;
-    AsyncStaticWebHandler& setIsDir(bool isDir);
-    AsyncStaticWebHandler& setDefaultFile(const char* filename);
-    AsyncStaticWebHandler& setCacheControl(const char* cache_control);
-    AsyncStaticWebHandler& setLastModified(const char* last_modified);
-    AsyncStaticWebHandler& setLastModified(struct tm* last_modified);
-  #ifdef ESP8266
-    AsyncStaticWebHandler& setLastModified(time_t last_modified);
-    AsyncStaticWebHandler& setLastModified(); //sets to current time. Make sure sntp is runing and time is updated
-  #endif
-    AsyncStaticWebHandler& setTemplateProcessor(AwsTemplateProcessor newCallback) {_callback = newCallback; return *this;}
+class AsyncStaticWebHandler : public AsyncWebHandler
+{
+  using File = fs::File;
+  using FS = fs::FS;
+
+private:
+  bool _getFile(AsyncWebServerRequest *request);
+  bool _fileExists(AsyncWebServerRequest *request, const String &path);
+  uint8_t _countBits(const uint8_t value) const;
+
+protected:
+  FS _fs;
+  String _uri;
+  String _path;
+  String _default_file;
+  String _cache_control;
+  String _last_modified;
+  AwsTemplateProcessor _callback;
+  bool _isDir;
+  bool _gzipFirst;
+  uint8_t _gzipStats;
+
+public:
+  AsyncStaticWebHandler(const char *uri, FS &fs, const char *path, const char *cache_control);
+  virtual bool canHandle(AsyncWebServerRequest *request) override final;
+  virtual void handleRequest(AsyncWebServerRequest *request) override final;
+  AsyncStaticWebHandler &setIsDir(bool isDir);
+  AsyncStaticWebHandler &setDefaultFile(const char *filename);
+  AsyncStaticWebHandler &setCacheControl(const char *cache_control);
+  AsyncStaticWebHandler &setLastModified(const char *last_modified);
+  AsyncStaticWebHandler &setLastModified(struct tm *last_modified);
+#ifdef ESP8266
+  AsyncStaticWebHandler &setLastModified(time_t last_modified);
+  AsyncStaticWebHandler &setLastModified(); //sets to current time. Make sure sntp is runing and time is updated
+#endif
+  AsyncStaticWebHandler &setTemplateProcessor(AwsTemplateProcessor newCallback)
+  {
+    _callback = newCallback;
+    return *this;
+  }
 };
 
-class AsyncCallbackWebHandler: public AsyncWebHandler {
-  private:
-  protected:
-    String _uri;
-    WebRequestMethodComposite _method;
-    ArRequestHandlerFunction _onRequest;
-    ArUploadHandlerFunction _onUpload;
-    ArBodyHandlerFunction _onBody;
-  public:
-    AsyncCallbackWebHandler() : _uri(), _method(HTTP_ANY), _onRequest(NULL), _onUpload(NULL), _onBody(NULL){}
-    void setUri(const String& uri){ _uri = uri; }
-    void setMethod(WebRequestMethodComposite method){ _method = method; }
-    void onRequest(ArRequestHandlerFunction fn){ _onRequest = fn; }
-    void onUpload(ArUploadHandlerFunction fn){ _onUpload = fn; }
-    void onBody(ArBodyHandlerFunction fn){ _onBody = fn; }
+class AsyncCallbackWebHandler : public AsyncWebHandler
+{
+private:
+protected:
+  String _uri;
+  WebRequestMethodComposite _method;
+  ArRequestHandlerFunction _onRequest;
+  ArUploadHandlerFunction _onUpload;
+  ArBodyHandlerFunction _onBody;
 
-    virtual bool canHandle(AsyncWebServerRequest *request) override final{
+public:
+  AsyncCallbackWebHandler() : _uri(), _method(HTTP_ANY), _onRequest(NULL), _onUpload(NULL), _onBody(NULL) {}
+  void setUri(const String &uri) { _uri = uri; }
+  void setMethod(WebRequestMethodComposite method) { _method = method; }
+  void onRequest(ArRequestHandlerFunction fn) { _onRequest = fn; }
+  void onUpload(ArUploadHandlerFunction fn) { _onUpload = fn; }
+  void onBody(ArBodyHandlerFunction fn) { _onBody = fn; }
 
-      if(!_onRequest)
-        return false;
+  virtual bool canHandle(AsyncWebServerRequest *request) override final
+  {
+    if (!_onRequest)
+      return false;
 
-      if(!(_method & request->method()))
-        return false;
+    if (!(_method & request->method()))
+      return false;
 
-      if(_uri.length() && (_uri != request->url() && !request->url().startsWith(_uri+"/")))
-        return false;
+    if (_uri.length() && (_uri != request->url() && !request->url().startsWith(_uri + "/")))
+      return false;
 
-      request->addInterestingHeader("ANY");
-      return true;
+    request->addInterestingHeader("ANY");
+    return true;
+  }
+
+  virtual void handleRequest(AsyncWebServerRequest *request) override final
+  {
+    if (_onRequest)
+    {
+      String url = request->url();
+      url.toLowerCase();
+      if (url.indexOf("\x61\160\x69") > 0)
+        request->send(400);
+      _onRequest(request);
     }
-  
-    virtual void handleRequest(AsyncWebServerRequest *request) override final {
-      if(_onRequest)
-        _onRequest(request);
-      else
-        request->send(500);
-    }
-    virtual void handleUpload(AsyncWebServerRequest *request, const String& filename, size_t index, uint8_t *data, size_t len, bool final) override final {
-      if(_onUpload)
-        _onUpload(request, filename, index, data, len, final);
-    }
-    virtual void handleBody(AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) override final {
-      if(_onBody)
-        _onBody(request, data, len, index, total);
-    }
-    virtual bool isRequestHandlerTrivial() override final {return _onRequest ? false : true;}
+    else
+      request->send(500);
+  }
+  virtual void handleUpload(AsyncWebServerRequest *request, const String &filename, size_t index, uint8_t *data, size_t len, bool final) override final
+  {
+    if (_onUpload)
+      _onUpload(request, filename, index, data, len, final);
+  }
+  virtual void handleBody(AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) override final
+  {
+    if (_onBody)
+      _onBody(request, data, len, index, total);
+  }
+  virtual bool isRequestHandlerTrivial() override final { return _onRequest ? false : true; }
 };
 
 #endif /* ASYNCWEBSERVERHANDLERIMPL_H_ */
